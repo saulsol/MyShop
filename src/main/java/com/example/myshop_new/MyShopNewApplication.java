d5ba882cd4cfdbe87cb94a952168b268cdb5d9a3
saul <dlathf3210@naver.com>
2023-04-22 20:52:55 +0900

    first commit

---
+++ src/main/java/com/example/myshop_new/MyShopNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.myshop_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myshop_new/MyShopNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshop_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
